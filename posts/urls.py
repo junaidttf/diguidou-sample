db607abc146d041b779fb97e8b46e156d427ef11
Moeez Ali <moeez4@gmail.com>
2024-11-29 16:11:39 +0500

    add new page + responsive design

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,11 @@ from posts import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
+    path('detail', views.DetailView.as_view(), name='detail'),
+    path('feed', views.FeedView.as_view(), name='feed'),
+    path('topic', views.TopicView.as_view(), name='topic'),
+    path('create-topic', views.CreateTopicView.as_view(), name='create-topic'),
+    path('profile', views.ProfileView.as_view(), name='profile'),
+    path('edit-profile', views.EditProfileView.as_view(), name='edit-profile'),
+    path('profile-setting', views.ProfileSettingView.as_view(), name='profile-setting'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,34 @@ from django.views import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'home.html')
+
+
+class DetailView(View):
+    def get(self, request):
+        return render(request, 'detail.html')
+
+
+class FeedView(View):
+    def get(self, request):
+        return render(request, 'feed.html')
+
+
+class TopicView(View):
+    def get(self, request):
+        return render(request, 'topic.html')
+
+class CreateTopicView(View):
+    def get(self, request):
+        return render(request, 'create-topic.html')
+
+class ProfileView(View):
+    def get(self, request):
+        return render(request, 'profile.html')
+
+class EditProfileView(View):
+    def get(self, request):
+        return render(request, 'edit-profile.html')
+
+class ProfileSettingView(View):
+    def get(self, request):
+        return render(request, 'profile-setting.html')
